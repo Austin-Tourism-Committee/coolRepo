659d2e798a743afa537c117f1634111cc018fbe6
Marissa Lucke <mjlncrew3115@gmail.com>
2025-10-09 20:45:46 -0500

    Added purple marker to location

--- map.js
+++ map.js
@@ -6,3 +6,15 @@
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+    // Define a custom purple icon
+    var purpleIcon = L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    // Add a purple marker to the map
+    var marker = L.marker([29.8884, -97.9384], { icon: purpleIcon }).addTo(map);
