63429eacef67a39acd8353731214a9abba39ce3c
Kai <backupemail99@yahoo.com>
2025-10-09 21:06:53 -0500

    Added line function to web map.

--- map.js
+++ map.js
@@ -1,14 +1,23 @@
-    var map = L.map('map').setView([29.8884, -97.9384], 14);
-    mapLink =
-        '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-    L.tileLayer(
-        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; ' + mapLink + ' Contributors',
-        maxZoom: 18,
-        }).addTo(map);
+var map = L.map('map').setView([29.8884, -97.9384], 14);
+mapLink =
+    '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+    }).addTo(map);
 
 
-    //    var from = turf.point([-75.343, 39.984]);
-  //      var to = turf.point([-75.534, 39.123]);
-  //      var options = { units: "miles" };
-  //      var distance = turf.distance(from, to, options);
+    var line = turf.lineString([
+      [-98.0000, 30.0000],
+      [-97.9384, 29.8884],
+      [-97.9000, 29.8000],
+    ]);
+    var length = turf.length(line, { units: "miles" })
+
+    L.geoJSON(line, {
+      style: {
+        color: 'red',
+        weight: 3
+      }
+    }).addTo(map);
