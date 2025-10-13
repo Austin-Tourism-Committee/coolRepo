63784ebb699698f4ec6efe0b79a13f869861534e
Seth Collinsworth <89612188+Gratocus@users.noreply.github.com>
2025-10-13 18:06:36 -0500

    Update createVoroniPolygons function

--- map.js
+++ map.js
@@ -7,16 +7,26 @@ L.tileLayer(
     maxZoom: 18,
     }).addTo(map);
 
-//This function creates a box with 100 random points via in it.
+//This function creates a box with 10 random points via in it.
 //Then the Voroni algorithm is used to created polygons around the points.
 function createVoroniPolygons() {
-  //Creates a box named borderBox that will be used as a boundary for the points
-  // to be generated in and for the voroni polygons to be made in.
+  //Creates a bounding box named borderBox that will be used as a boundary for
+  //the points to be generated in and for the voroni polygons to be made in.
   const borderBox = {
-    bbox: [-70, 40, -60, 60],
+    bbox: [-97.930957, 29.885643, -97.857865, 29.920151],
   };
-  //Creates 100 random points in borderBox.
-  const points = turf.randomPoint(100, borderBox);
+  //Creates 10 random points in borderBox.
+  const points = turf.randomPoint(10, borderBox);
   //Creates voroni polygons around each point inside borderBox.
   const voronoiPolygons = turf.voronoi(points, borderBox);
+
+  // Display the polygon on the map
+  L.geoJSON(voronoiPolygons, {
+    style: {
+      color: "#ed168b",     // border color
+      weight: 3,            // border thickness
+      fillColor: "#f9b9dc", // fill color
+      fillOpacity: 0.5      // fill transparency
+    }
+  }).addTo(map);
 }
